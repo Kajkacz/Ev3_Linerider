838a2400b53e5e7768e2c3b502ed8692ed453867
Kajetan Kaczmarek <kajkacz@wp.pl>
2018-04-12 17:34:38 +0200

    Changed checkGreen function and added newReturnToLine function

--- Linerider.py
+++ Linerider.py
@@ -5,137 +5,9 @@ from time import *
 ## Skrypt który nie realizuje transportu a tylko jedzie po lini
 ##################################
 
-
 def colorSum(colorSensor):
 	color = colorSensor.value(0) + colorSensor.value(1) + colorSensor.value(2)
 	return color	
-
-def countTheta(color_sensor1, color_sensor2): #Nieużywane (?)
-	theta = (2*color_sensor1.value(0) - color_sensor1.value(1) - color_sensor1.value(2) + 2*color_sensor2.value(0) - color_sensor2.value(1) - color_sensor2.value(2))/2
-	return theta
-	
-def checkRed():
-	theta = (cl_left.value(0) - cl_left.value(1) + cl_left.value(0) - cl_left.value(2))
-	print("The red is " , cl_left.value(0) , " And the sum is " , colorSum(cl_left))
-	
-	return False
-	#DOPASOWAĆ WARTOŚCI NIŻEJ, to jest tylko kalka z zielonego!I usunąć return wyżej
-	
-	if((lowerRedThreshold < cl_left.value(0) < upperRedThreshold ) and (lowerSumRedThreshold<colorSum(cl_left)<upperSumRedThreshold) and (theta < upperThetaRedThreshold)): # or (90 < cl_right.value(1) < 160 )):# and countTheta(cl_left,cl_right) < -45):
-		return True
-	else:
-		return False
-
-
-def checkGreen():
-	theta = (cl_left.value(0) - cl_left.value(1) + cl_left.value(0) - cl_left.value(2))
-	print("The green is " , cl_left.value(1) , " And the sum is " , colorSum(cl_left))
-	
-	if((lowerGreenThreshold < cl_left.value(1) < upperGreenThreshold ) and (lowerSumGreenThreshold<colorSum(cl_left)<upperSumGreenThreshold) and (theta < upperThetaGreenThreshold)): # or (90 < cl_right.value(1) < 160 )):# and countTheta(cl_left,cl_right) < -45):
-		return True
-	else:
-		return False
-	
-def leftGreen(): #To służy do sprawdzania strony, raczej do śmieci
-	if(90 < cl_left.value(1) < 150 and countTheta(cl_left,cl_right) < -45):
-		return True
-	if(90 < cl_right.value(1) < 150 and countTheta(cl_left,cl_right) < -45):
-		return False
-	return 0
-
-def returnToLine(): 							# Funkcja wracająca robota na linię jazdy
-	left_motor.run_forever(speed_sp = -power)	# Obracamy się o 180 stopni w kierunku do lini
-	right_motor.run_forever(speed_sp = power)
-	sleep(2*turningTimeRightAngle) 
-	
-	left_motor.run_forever(speed_sp = power)	# Dojeżdżamy do środka
-	right_motor.run_forever(speed_sp = power)
-	sleep(forwardTimeToCentre)
-	
-	left_motor.run_forever(speed_sp = power)	# Obracamy się o 90 stopni w kierunku jazdy
-	right_motor.run_forever(speed_sp = -power)
-	sleep(turningTimeRightAngle) 
-			
-def pickUpNaive():									# Naiwna werjsa podnoszenia 
-	left_motor.run_forever(speed_sp = power)	# Obracamy się o 90 stopni w kierunku do pola podnoszenia
-	right_motor.run_forever(speed_sp = -power)
-	sleep(turningTimeRightAngle) 
-	
-	left_motor.run_forever(speed_sp = power)	# Dojeżdżamy do środka
-	right_motor.run_forever(speed_sp = power)
-	sleep(forwardTimeToCentre)
-	
-	left_motor.run_forever(speed_sp = 0)		# Zatrzymujemy silnik
-	right_motor.run_forever(speed_sp = 0)
-	
-	mB.run_forever(speed_sp = -70)				# Podnosimy klocek
-	sleep(3)
-	mB.run_forever(stop_action = "hold") #Jeśli to nie działa to cofnąć do wersji niżej
-	#mB.run_forever(speed_sp = 0)
-	
-	returnToLine()								# Wracamy do Lini jazdy
-		
-def pickUp():
-	left_motor.run_forever(speed_sp = power)
-	right_motor.run_forever(speed_sp = -power)
-	blockPickedUp = False
-	
-	while(not blockPickedUp):
-			while(ifs.value()>23 and not blockPickedUp):
-				left_motor.run_forever(speed_sp = -turningSpeed)
-				right_motor.run_forever(speed_sp = turningSpeed)
-			#print(ifs.value())
-			left_motor.run_forever(speed_sp = turningSpeed)
-			right_motor.run_forever(speed_sp = turningSpeed)
-			if(ifs.value()<2 and not blockPickedUp):
-				left_motor.run_forever(speed_sp = 0)
-				right_motor.run_forever(speed_sp = 0)
-				mB.run_forever(speed_sp = -70)
-				sleep(3)
-				mB.run_forever(speed_sp = 0)
-				#mB.hold()
-				blockPickedUp=True
-				remember = colorSum(cl_right) + colorSum(cl_left)
-				while(( colorSum(cl_right) + colorSum(cl_left) ) - remember < 200):
-					remember = colorSum(cl_right) + colorSum(cl_left)
-					print(remember)
-					#print( colorSum(cl_right) + colorSum(cl_left))
-					left_motor.run_forever(speed_sp = -turningSpeed)
-					right_motor.run_forever(speed_sp = -turningSpeed)
-				while(colorSum(cl_right)<200 or colorSum(cl_left)<200 ):
-					left_motor.run_forever(speed_sp = turningSpeed)
-					right_motor.run_forever(speed_sp = -turningSpeed)
-				#left_motor.run_forever(speed_sp = -turningSpeed)
-				#right_motor.run_forever(speed_sp = +turningSpeed)
-				#sleep(0.75)
-				#left_motor.run_forever(speed_sp = 0)
-				#right_motor.run_forever(speed_sp = 0)
-				break	
-		
-
-def dropBlock(): 
-	left_motor.run_forever(speed_sp = power)	# Obracamy się o 90 stopni w kierunku do pola podnoszenia
-	right_motor.run_forever(speed_sp = -power)
-	sleep(turningTimeRightAngle) 
-	
-	left_motor.run_forever(speed_sp = power)	# Dojeżdżamy do środka
-	right_motor.run_forever(speed_sp = power)
-	sleep(forwardTimeToCentre)
-	
-	left_motor.run_forever(speed_sp = 0)		# Zatrzymujemy silnik
-	right_motor.run_forever(speed_sp = 0)
-	
-	mB.run_forever(speed_sp = 70)				# Opuszczamy klocek
-	sleep(3)
-	mB.run_forever(stop_action = "hold") #Jeśli to nie działa to cofnąć do wersji niżej
-	#mB.run_forever(speed_sp = 0)
-	
-	# Jeśli nie musimy wracać na tor to poniższy kod jest zbędny
-	returnToLine()
-	
-	
-
-	
 
 #Steering module alt. 2: "less aggressive". A less aggressive algorithm that adjusts the power on each motor from 0-100% (never reverse)
 def steering(course, power):
@@ -164,7 +36,7 @@ def run(power, target, kp, kd, ki, minRef, maxRef): #Wersja "Poprawiona"
 	lastError = error = integral = 0
 	left_motor.run_forever(speed_sp = power)
 	right_motor.run_forever(speed_sp = power) 
-	blockPickedUp = False # Flaga o podniesieniu klocka
+	
 	while(True): #not btn.any() :
 		#print(ifs.value())
 		if ts.value():
@@ -180,24 +52,6 @@ def run(power, target, kp, kd, ki, minRef, maxRef): #Wersja "Poprawiona"
 		left_motor.run_forever(speed_sp = power_left)
 		right_motor.run_forever(speed_sp = power_right)	
 		sleep(0.01)
-
-#Stałe do obrotu i ruchu przy podnoszeniu
-turningTimeRightAngle = 1 					# Trzeba zahardcode'ować żeby obracał się o kąt prosty.Nie wiem jak inaczej, w sumie chyba się nie da
-forwardTimeToCentre = 1					 	# Jak wyżej, tutaj dajemy wartość taką żeby dojechał do środka czerwonego pola
-
-#Stałe do wykrywania kolorów
-	#Zielony
-lowerGreenThreshold = 100		# Dolna granica wartości G
-upperGreenThreshold = 180		# Górna granica wartości G
-lowerSumGreenThreshold = 140	# Dolna granica sumy wartości
-upperSumGreenThreshold = 300	# Górna granica sumy wartości
-upperThetaGreenThreshold = -60	# Górna granica pomocniczej zmiennej Theta
-	# Czerwony
-lowerGreenThreshold = 0			# jw. - wypełnić wartości dla czerwonego
-upperGreenThreshold = 0
-lowerSumGreenThreshold = 0
-upperSumGreenThreshold = 0
-upperThetaGreenThreshold = 0
 
 #Engine Initialization
 
@@ -243,8 +97,7 @@ left_green = False
 green = False
 
 run(power, target, kp, kd, ki, minRef, maxRef)
-# TO wyżej to nowa wersja, niżej siedzi stara
-#runCycle()
+
 print ('Stopping motors')
 left_motor.run_forever(speed_sp = stopSpeed)
 right_motor.run_forever(speed_sp = stopSpeed)
--- Transporter.py
+++ Transporter.py
@@ -25,16 +25,9 @@ ki = float(0.02)
 def colorSum(colorSensor):						#Funkcja licząca sumę kolorów w danym miejscu dla podanego sensora
 	color = colorSensor.value(0) + colorSensor.value(1) + colorSensor.value(2)
 	return color	
-
-def countTheta(color_sensor1, color_sensor2): 	#Nieużywane (?)
-	theta = (2*color_sensor1.value(0) - color_sensor1.value(1) - color_sensor1.value(2) + 2*color_sensor2.value(0) - color_sensor2.value(1) - color_sensor2.value(2))/2
-	return theta
 	
 def checkRed(colorSensor):						# Funkcja sprawdzająca czy obecny odczyt wpada w zakres czerwonego
-	lowerRedThreshold = 215 					# jak w zielonym . - wypełnić wartości dla czerwonego
-	upperRedThreshold = 250
-	lowerSumRedThreshold = 280
-	upperSumRedThreshold = 320
+												
 	lowerThetaRedThreshold = 250				# Jako że nasza Theta jest liczona dla podwójnej wartości czerwonego to po testach zacząłem używać
 												# tylko tej wartości do sprawdzania czy jestem na polu czerwonym
 	theta = (colorSensor.value(0) - colorSensor.value(1) + colorSensor.value(0) - colorSensor.value(2))
@@ -48,32 +41,17 @@ def checkRed(colorSensor):						# Funkcja sprawdzająca czy obecny odczyt wpada 
 
 
 def checkGreen():								# Funkcja sprawdzająca czy obecny odczyt wpada w zakres czerwonego
-	
-	#Stałe do wykrywania kolorów
-	
 		#Zielony
-	lowerGreenThreshold = 160		# Dolna granica wartości G
-	upperGreenThreshold = 190		# Górna granica wartości G
-	lowerSumGreenThreshold = 230	# Dolna granica sumy wartości
-	upperSumGreenThreshold = 350	# Górna granica sumy wartości
-	upperThetaGreenThreshold = -60	# Górna granica pomocniczej zmiennej Theta
-	
-	theta = (cl_left.value(0) - cl_left.value(1) + cl_left.value(0) - cl_left.value(2))
-	#print("The green is " , cl_left.value(1) , " , the sum is " , colorSum(cl_left) ,"and the theta is" , theta)	#Print readings
-	#print(lowerGreenThreshold ,upperGreenThreshold,lowerSumGreenThreshold ,upperSumGreenThreshold, theta )			# Print thresholds
-	if((lowerGreenThreshold < cl_left.value(1)) and (cl_left.value(1) < upperGreenThreshold ) and (lowerSumGreenThreshold<colorSum(cl_left)) and (colorSum(cl_left)<upperSumGreenThreshold) and (theta < upperThetaGreenThreshold)): # or (90 < cl_right.value(1) < 160 )):# and countTheta(cl_left,cl_right) < -45):
-		print("I am in, the green is " , cl_left.value(1) , " , the sum is " , colorSum(cl_left) ,"and the theta is" , theta)	#Print readings
+	lowerThetaGreenThreshold = 280	# Dolna granica pomocniczej zmiennej Theta - jako że dla czerwonego działało to bardzo dobrze zmieniam warunek
+									# tak aby sprawdzał wg. zmienionej wersji theta, czyli 2*G - R - B. Myślę że może to dobrze zadziałać
+	theta = (2*cl_left.value(1) - cl_left.value(0) - cl_left.value(2))
+	#print("The red is " , cl_left.value(0) , " and the sum is " , colorSum(cl_left), "and the Theta is " , theta)
+	if(theta < upperThetaGreenThreshold):
+		#print("I am in, the green is " , cl_left.value(1) , " , the sum is " , colorSum(cl_left) ,"and the theta is" , theta)	#Print readings
 		return True
 	else:
 		return False
-	
-def leftGreen(): #To służy do sprawdzania strony, raczej do śmieci
-	if(90 < cl_left.value(1) < 150 and countTheta(cl_left,cl_right) < -45):
-		return True
-	if(90 < cl_right.value(1) < 150 and countTheta(cl_left,cl_right) < -45):
-		return False
-	return 0
-
+		
 def returnToLine(): 							# Funkcja wracająca robota na linię jazdy
 	global forwardTimeToCentre
 	global turningTimeRightAngle
@@ -86,6 +64,35 @@ def returnToLine(): 							# Funkcja wracająca robota na linię jazdy
 	left_motor.run_forever(speed_sp = power)	# Obracamy się o 90 stopni w kierunku jazdy
 	right_motor.run_forever(speed_sp = -power)
 	sleep(turningTimeRightAngle) 
+
+def newReturnToLine():
+		# Wartości pomocnicze aby sprawdzić czy wracamy z pola zielonego czy czerwonego
+	thetaRed = ( 2*colorSensor.value(0) - colorSensor.value(1)  - colorSensor.value(2))
+	thetaGreen = ( 2*colorSensor.value(1) - colorSensor.value(0) - colorSensor.value(2))
+	diff = 200 # Pomocniczy parametr - oznacza różnicę pomiędzy thetami jaka musi wystąpić żeby uznać że dojechaliśmy do czarnego
+	sumThreshold = 200 # Jaka musi być suma aby uznać że jest czarna
+	isRed = isGreen = False
+	
+	if(thetaRed > thetaGreen):
+		isRed = True
+	else:
+		isGreen = True
+		
+	previousSum = colorSum(cl_right) + colorSum(cl_left)
+	
+	while(( colorSum(cl_right) + colorSum(cl_left) ) - previousSum < sumThreshold or abs(thetaRed - thetaGreen) > diff): # Wracamy do lini dla dużego skoku wartości
+		previousSum = colorSum(cl_right) + colorSum(cl_left)
+		thetaRed = ( 2*colorSensor.value(0) - colorSensor.value(1)  - colorSensor.value(2))
+		thetaGreen = ( 2*colorSensor.value(1) - colorSensor.value(0) - colorSensor.value(2))
+		
+		#print("Previous sum is " , previousSum , " the theta for red is " , thetaRed , " and the theta for green is " , thetaGreen)
+
+		left_motor.run_forever(speed_sp = -turningSpeed)
+		right_motor.run_forever(speed_sp = -turningSpeed)
+		
+	while(colorSum(cl_right)<sumThreshold or colorSum(cl_left)<sumThreshold ):			# Obracamy robota w kierunku jazdy
+		left_motor.run_forever(speed_sp = turningSpeed)
+		right_motor.run_forever(speed_sp = -turningSpeed)
 
 def pickUp():									# Funkcja podnosząca klocek
 	#sleep(1)
@@ -104,25 +111,19 @@ def pickUp():									# Funkcja podnosząca klocek
 			right_motor.run_forever(speed_sp = turningSpeed)
 			arrivedAtBlock = 2
 			if(ifs.value() < arrivedAtBlock and not blockPickedUp): # Kiedy zbliżymy się na odległość zdefiniowaną powyżej (czyli de facto dotkniemy klocka) podnosimy widełki
-				left_motor.run_forever(speed_sp = 0)
+				left_motor.run_forever(speed_sp = 0)		# Stop
 				right_motor.run_forever(speed_sp = 0)
-				mB.run_forever(speed_sp = -70)
+				
+				mB.run_forever(speed_sp = -70)				# Podnieś
 				sleep(3)
-				mB.run_forever(speed_sp = 0)
-				
-				blockPickedUp=True								# Zapamiętujemy podniesienie klocka
-				
-				remember = colorSum(cl_right) + colorSum(cl_left)
-				while(( colorSum(cl_right) + colorSum(cl_left) ) - remember < 200): # Wracamy do lini
-					remember = colorSum(cl_right) + colorSum(cl_left)
-					print(remember)
-					#print( colorSum(cl_right) + colorSum(cl_left))
-					left_motor.run_forever(speed_sp = -turningSpeed)
-					right_motor.run_forever(speed_sp = -turningSpeed)
+				mB.run_forever(stop_action = "hold")		# Jeśli to nie działa to cofnąć do wersji niżej - trzymanie klocka w górze
+				#mB.run_forever(speed_sp = 0)
 					
-				while(colorSum(cl_right)<200 or colorSum(cl_left)<200 ):			# Obracamy robota w kierunku jazdy
-					left_motor.run_forever(speed_sp = turningSpeed)
-					right_motor.run_forever(speed_sp = -turningSpeed)
+				blockPickedUp=True							# Zapamiętujemy podniesienie klocka
+				
+				#returnToLine() #Stare i pewne
+				newReturnToLine()							# Wracamy do lini
+				
 				break	
 		
 
@@ -138,7 +139,7 @@ def dropBlock(): 								# Funkcja upuszczająca klocek
 	left_motor.run_forever(speed_sp = power)	# Dojeżdżamy do środka
 	right_motor.run_forever(speed_sp = power)
 	
-	while(not (checkRed(cl_left)  and checkRed(cl_right))):
+	while(not (checkRed(cl_left)  and checkRed(cl_right))):	# Wykrywamy środek kiedy dwa sensory widzą czerwony
 		print("Looking for red")
 	left_motor.run_forever(speed_sp = 0)		# Zatrzymujemy silnik
 	right_motor.run_forever(speed_sp = 0)
@@ -149,7 +150,8 @@ def dropBlock(): 								# Funkcja upuszczająca klocek
 	#mB.run_forever(speed_sp = 0)
 	
 	# Jeśli nie musimy wracać na tor to poniższy kod jest zbędny
-	returnToLine()
+	#returnToLine() #Stare i pewne
+	newReturnToLine()
 
 	
 
@@ -178,7 +180,7 @@ def steering(course, power):
 	return (int(power_left), int(power_right))
 
 
-def runv2(power, target, kp, kd, ki, minRef, maxRef): #Wersja "Poprawiona" 
+def run(power, target, kp, kd, ki, minRef, maxRef): #Wersja "Poprawiona" 
 	lastError = error = integral = 0
 	left_motor.run_forever(speed_sp = power)
 	right_motor.run_forever(speed_sp = power) 
@@ -207,39 +209,6 @@ def runv2(power, target, kp, kd, ki, minRef, maxRef): #Wersja "Poprawiona"
 		right_motor.run_forever(speed_sp = power_right)	
 		sleep(0.01)
 
-def run(power, target, kp, kd, ki, minRef, maxRef): #Wersja standardowa
-	lastError = error = integral = 0
-	left_motor.run_forever(speed_sp = power)
-	right_motor.run_forever(speed_sp = power)
-	blockPickedUp = False # Flaga o podniesieniu klocka
-	while(True): #not btn.any() :
-		#print(ifs.value())
-		if ts.value():
-			print ('Breaking loop') # User pressed touch sensor
-			break
-		if (checkGreen() == True and not blockPickedUp):
-			break
-		if (checkRed(cl_left) == True and blockPickedUp):
-			break
-		refRead = (colorSum(cl_right) + colorSum(cl_left))/2
-		error = target - (100 * ( refRead - minRef ) / ( maxRef - minRef ))
-		derivative = error - lastError
-		lastError = error
-		integral = float(0.5) * integral + error
-		course = kp * error + kd * derivative +ki * integral
-		power_left, power_right = steering(course, power)
-		left_motor.run_forever(speed_sp = power_left)
-		right_motor.run_forever(speed_sp = power_right)	
-		sleep(0.01)
-def runCycle():
-	run(power, target, kp, kd, ki, minRef, maxRef)
-	print("I'm going for the pickUp")
-	pickUp()
-	run(power, target, kp, kd, ki, minRef, maxRef)
-	print("I'm going for the Drop")
-	dropBlock()
-	run(power, target, kp, kd, ki, minRef, maxRef) # To jeśli ma gdzieś jechać po upuszczeniu
-
 #Engine Initialization
 
 mB = MediumMotor('outB')
@@ -267,9 +236,8 @@ ts = TouchSensor('in4')
 
 #btn = Button()
 
-runv2(power, target, kp, kd, ki, minRef, maxRef)
-# TO wyżej to nowa wersja, niżej siedzi stara
-#runCycle()
+run(power, target, kp, kd, ki, minRef, maxRef) # Główna funkcja kontrolująca jazdę
+
 print ('Stopping motors')
 left_motor.run_forever(speed_sp = stopSpeed)
 right_motor.run_forever(speed_sp = stopSpeed)
